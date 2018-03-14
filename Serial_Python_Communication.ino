ca180ddf7363eaaf58d6d6d6153095bb930d0475
Unknown <quentingauthier@gmail.com>
2018-03-14 16:06:53 -0700

    Added files

---
+++ Serial_Python_Communication.ino
@@ -0,0 +1,49 @@
+#include <Wire.h>
+#include <LCD.h>
+#include <LiquidCrystal_I2C.h> // F Malpartida's NewLiquidCrystal library
+#define I2C_ADDR 0x3F // Define I2C Address for controller
+#define En_pin 2
+#define Rw_pin 1
+#define Rs_pin 0
+#define D4_pin 4
+#define D5_pin 5
+#define D6_pin 6
+#define D7_pin 7
+#define BACKLIGHT 3
+
+LiquidCrystal_I2C lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
+
+void setup() {
+
+// Switch on the backlight
+lcd.setBacklightPin(BACKLIGHT,POSITIVE);
+lcd.setBacklight(HIGH);
+
+// Reset the display
+lcd.begin(16, 2);
+lcd.clear();
+lcd.home();
+lcd.print("start");
+ 
+//Serial.begin(115200);
+Serial.begin(9600);
+
+}
+ 
+void loop() {
+  while (Serial.available()) {
+    lcd.begin(16,2);
+    lcd.clear();
+    lcd.home();
+    Serial.println("");
+    Serial.println("TL1");
+    delay(100);  //wait some time for the data to fully be read
+    Serial.println("TL2");
+    while (Serial.available() > 0) {
+      char c = Serial.read();
+      lcd.write(c);
+      Serial.print(c);
+      delay(2);  //wait some time for the data to fully be read
+    }
+  }
+}
